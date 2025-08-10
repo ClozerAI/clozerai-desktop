9b66794f2f6e22a114b1f6166c28e0f1e97154a6
Jure Sotosek <jure.sotosek@gmail.com>
2025-08-10 17:26:26 +0200

    fix crash

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -472,7 +472,7 @@ export default function App() {
       setUpdateError(null);
     };
 
-    const updateAvailableHandler = (...args: unknown[]) => {
+    const updateAvailableHandler = () => {
       setUpdateStatus('available');
       setUpdateError(null);
     };
