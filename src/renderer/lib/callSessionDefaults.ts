fa3a93f6722a2cae9c9870f59750e396b977c369
Jure Sotosek <jure.sotosek@gmail.com>
2025-09-04 20:36:13 +0200

    add background noice and new create screen

--- src/renderer/lib/callSessionDefaults.ts
+++ src/renderer/lib/callSessionDefaults.ts
@@ -3,4 +3,4 @@ import { TranscriptionLanguage } from './transcriptLanguageMap';
 export const DEFAULT_LANGUAGE = TranscriptionLanguage.English;
 export const DEFAULT_AI_MODEL = 'gpt-4.1-mini';
 export const DEFAULT_SAVE_TRANSCRIPTION = true;
-export const DEFAULT_BACKGROUND_FILTERING = 3;
+export const DEFAULT_BACKGROUND_FILTERING = 1.5;
